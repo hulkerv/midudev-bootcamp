5f21adf6f272a61c5c1758733ffa539d3f518c51
Hulker <vegat.kevinj@gmail.com>
2021-05-07 18:50:18 -0400

    courseinfo finished

--- part1/exercises/courseinfo/src/App.js
+++ part1/exercises/courseinfo/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+const Title = ({course}) => <h1>{course}</h1>;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Indice123 = (props)=>{
+    const {part, exercises} = props
+    return <p>{part} {exercises}</p>
 }
 
-export default App;
+const Indice4 = ({totalExercises}) => <p>Number of exercises {totalExercises}</p>;
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const totalExercises = exercises1 + exercises2 + exercises3
+  return (
+    <div>
+      <Title course={course}/>
+      
+      <Indice123 part={part1} exercises={exercises1}/>
+      
+      <Indice123 part={part2} exercises={exercises2}/>
+      
+      <Indice123 part={part3} exercises={exercises3}/>
+
+      <Indice4 totalExercises={totalExercises}/>
+    </div>
+  )
+}
+
+export default App
