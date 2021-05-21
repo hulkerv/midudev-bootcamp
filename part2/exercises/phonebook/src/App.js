484237a3f36c57b91e4f80133b2c890b4ebc891b
Hulker <vegat.kevinj@gmail.com>
2021-05-21 08:59:06 -0400

    part2/exercises/phonebook: phonebook step 3

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -2,31 +2,46 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+		name: 'Arto Hellas',
+	 	number:'04241234567'
+	}
   ]); 
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
-  	const handleChange = (e) => {
-		setNewName(e.target.value)
-	};
+  	const handleChange = {
+		name: (e) => {
+			setNewName(e.target.value)
+		},
+		number: (e) => {
+			setNewNumber(e.target.value)
+		}
+	}
 	
 	const handleSubmit = (e) => {
 		
 		e.preventDefault()
 		
 		const namesArray = persons.map((person)=>(person.name));
+		const numbersArray = persons.map((person)=>(person.number));
 		
-		const findName = namesArray.indexOf(newName)
+		const findName = namesArray.indexOf(newName);
+		const findNumber = numbersArray.indexOf(newNumber);
 				
 		if(namesArray[findName] === newName){
-			return alert(`${newName} is already exist` )
+			return alert(`${newName} is already exist` );
+		}else if(numbersArray[findNumber] === newNumber){
+			return alert(`The number ${newNumber} is already exist` );
 		}
 		const nameToAddToState = {
-			name: newName
+			name: newName,
+			number: newNumber
 			};
 		
 			setPersons(prevSetPersons => prevSetPersons.concat(nameToAddToState));
 			setNewName('');
+			setNewNumber('');
 	};
 	
   return (
@@ -34,7 +49,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <input onChange={handleChange} type="text" placeholder="Add Name..." value={newName}/>
+          <input id="name" onChange={handleChange.name} type="text" placeholder="Add Name..." value={newName}/>
+          <input id="number" onChange={handleChange.number} type="number" placeholder="Add Number..." value={newNumber}/>
         </div>
         <div>
           <button>add</button>
@@ -42,7 +58,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-      	{persons.map((person)=><p key={person.name}>{person.name}</p>)}
+      	{persons.map((person)=><p key={person.number}>{person.name} - {person.number}</p>)}
       </div>
     </div>
   )
